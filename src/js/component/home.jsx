779f681ce50dc426d51bb00a54064b50afaa47d3
ipeters123 <isaiah.peters64@gmail.com>
2023-03-02 04:16:01 +0000

    commentation

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
   const [isShown, setIsShown] = useState(-1);
   const [done, setDone] = useState(true)
  
-
+// define tasks using useEffect //
   useEffect(() => {
     fetch(apiURL)
       .then((result) => {
